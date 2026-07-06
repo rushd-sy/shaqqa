44c0581311b5d9c7ce03aa5e6a539101b47fef93
Ahmad7757 <ahmadbesherdabbagh@gmail.com>
2026-07-06 18:03:11 +0300

    fix : resolve comments

--- Backend/src/RealEstate.Domain/Common/AuditableEntity.cs
+++ Backend/src/RealEstate.Domain/Common/AuditableEntity.cs
@@ -1,7 +1,13 @@
 namespace RealEstate.Domain.Common;
-public abstract class AuditableEntity
+public abstract class AuditableEntity : Entity
 {
+     protected AuditableEntity(Guid id) : base(id)
+     {
+            
+     }
+
      public DateTimeOffset CreatedAtUtc { get; set; }
+
      public string? CreatedBy { get; set; }
 
      public DateTimeOffset LastModifiedUtc { get; set; }
--- Backend/src/RealEstate.Domain/Common/Entity.cs
+++ Backend/src/RealEstate.Domain/Common/Entity.cs
@@ -3,23 +3,24 @@ public abstract class Entity
 {
     public Guid Id { get;  } 
     
-    private readonly List<DomainEvent> _domainEvent = [];
-    protected Entity()
-    {
-        
-    }
-      protected Entity(Guid id)
+    private readonly List<DomainEvent> _domainEvents = [];
+
+    protected Entity(Guid id)
     {
         Id = id == Guid.Empty ? Guid.NewGuid(): id;
+    }
+    public void AddDomainEvent(DomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
     }
 
     public void RemoveDomainEvent(DomainEvent domainEvent)
     {
-        _domainEvent.Remove(domainEvent);
+        _domainEvents.Remove(domainEvent);
     }
    public void ClearDomainEvent()
     {
-        _domainEvent.Clear();
+        _domainEvents.Clear();
     }
   
 }
